8e4b7d9100eb8791ef2f38b61b24105d28ddd552
queenuski <elenafrm@hotmail.com>
2016-06-01 08:12:14 +0200

    Add files via upload
    Segunda versión del robot sigue líneas

--- MyAlgorithm.py
+++ MyAlgorithm.py
@@ -15,6 +15,43 @@ class MyAlgorithm():
         self.lock = threading.Lock()
         self.count = 0
 
+    def cosa():
+        px_dcha = 0
+        px_izqda = 0
+
+        if (ROI_inf_gray[239,320]>0):
+            line = 1
+            self.sensor.setW(0.0)
+
+            primero = 1
+            ultimo = 1
+            j = ancho - 1
+            #print ancho
+            for i in range(ancho):
+                print ROI_inf_gray[1,i]
+                if (ROI_inf_gray[0,i] > 0 & primero==1):
+                    px_dcha = i
+                    print px_dcha
+                    primero = 0
+                if (ROI_inf_gray[0,j] > 0 & ultimo==1):
+                    px_izqda = j
+                    print px_izqda
+                    ultimo = 0
+                j = j - 1
+
+            #print px_dcha, px_izqda
+
+            lado = 10
+            cv2.rectangle(final,(px_dcha,alto),(px_dcha+lado,alto-lado),(0,0,255),-1)
+            cv2.rectangle(final,(px_izqda,alto),(px_izqda+lado,alto-lado),(0,0,255),-1)
+
+            # final [0:5,i:i+5] = [0,0,150]
+            # final [0:5,j:j+5] = [0,0,150]
+
+        else:
+            self.sensor.setV(0)
+            self.sensor.setW(0.05)
+
     def colorFilter(self, image):
         # made all red pixels = 0
         # img[:,:,2]=0
@@ -81,53 +118,60 @@ class MyAlgorithm():
         kernel = np.ones((20,20),np.uint8)
         erosion = cv2.erode(ROI_inf_gray,kernel,iterations = 1)
 
+	#cv2.imshow("erosion", erosion)
+	
+	dibujo = erosion.copy()
+	
+	ret,thresh = cv2.threshold(dibujo,127,255,0)
+	
+	#cv2.imshow("thresh", thresh)
+	
+	contours,hierarchy = cv2.findContours(thresh, 1, 2)
+	print type(contours)
+ 
+	if contours:
+		print "List is empty"
 
-        cv2.imshow("ROI_inf", ROI_inf_gray)
+		cnt = contours[0]
+	
+		M = cv2.moments(cnt)
+ 		
+ 		if M['m00'] != 0:
+ 			print "M['m00']:  ",M['m00']
+ 		  
+			cx = int(M['m10']/M['m00'])
+			cy = int(M['m01']/M['m00'])
+	
+			ancho = np.size(dibujo, 0)
+			largo = np.size(dibujo, 0)
 
-        cv2.imshow("erosion", erosion)
+			imgFinal = imageLeft.copy()
+
+			if (cx > 0 & cx < ancho & cy > 0 & cy < largo):
+				radius = 5
+				color = (0,255,0)
+				cv2.circle(dibujo, (cx,cy), radius, color, -1)
+				cv2.circle(imgFinal, (cx,cy+240), radius, color, -1)
+
+			cv2.imshow("dibujo", dibujo)
+
+			cv2.imshow("imgFinal", imgFinal)
+
+        #cv2.imshow("ROI_inf", ROI_inf_gray)
+
+        
 
         alto, ancho, _z = imageLeft.shape
 
         final = imageLeft
 
-        px_dcha = 0
-        px_izqda = 0
-
-        if (ROI_inf_gray[239,320]>0):
-            line = 1
-            self.sensor.setW(0.0)
-
-            primero = 1
-            ultimo = 1
-            j = ancho - 1
-            #print ancho
-            for i in range(ancho):
-                print ROI_inf_gray[1,i]
-                if (ROI_inf_gray[0,i] > 0 & primero==1):
-                    px_dcha = i
-                    print px_dcha
-                    primero = 0
-                if (ROI_inf_gray[0,j] > 0 & ultimo==1):
-                    px_izqda = j
-                    print px_izqda
-                    ultimo = 0
-                j = j - 1
-
-            #print px_dcha, px_izqda
-
-            lado = 10
-            cv2.rectangle(final,(px_dcha,alto),(px_dcha+lado,alto-lado),(0,0,255),-1)
-            cv2.rectangle(final,(px_izqda,alto),(px_izqda+lado,alto-lado),(0,0,255),-1)
-
-            # final [0:5,i:i+5] = [0,0,150]
-            # final [0:5,j:j+5] = [0,0,150]
-
-        else:
-            self.sensor.setV(0)
-            self.sensor.setW(0.05)
+	self.sensor.setV(0)
+        self.sensor.setW(0.05)
 
 
-        cv2.imshow("final", final)
+
+
+        # cv2.imshow("final", final)
 
         #SHOW THE FILTERED IMAGE ON THE GUI
         self.setRightImageFiltered(imageLeft)
